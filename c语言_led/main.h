94911f254b12026d3eb29f2d9c0019f9c2b10558
gaohu-0228 <1805662947@qq.com>
2022-08-29 23:08:34 +0800

    添加启动文件以及main.c和main.h文件

---
+++ c语言_led/main.h
@@ -0,0 +1,35 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/*
+ * CCM相关寄存器地址
+ */
+#define CCM_CCGR0       *((volatile unsigned int*))
+#define CCM_CCGR0       *((volatile unsigned int*))
+#define CCM_CCGR0       *((volatile unsigned int*))
+#define CCM_CCGR0       *((volatile unsigned int*))
+#define CCM_CCGR0       *((volatile unsigned int*))
+#define CCM_CCGR0       *((volatile unsigned int*))
+#define CCM_CCGR0       *((volatile unsigned int*))
+
+/*
+ * IOMUX相关寄存器地址
+ */
+#define SW_MUX_GPIO1_IO03       *((volatile unsigned int*))
+#define SW_PAD_GPIO1_IO03       *((volatile unsigned int*))
+
+/*
+ * GPIO1相关寄存器地址
+ */
+#define GPIO1_DR        *((volatile unsigned int*))
+#define GPIO1_GDIR      *((volatile unsigned int*))
+#define GPIO1_PSR       *((volatile unsigned int*))
+#define GPIO1_ICR1      *((volatile unsigned int*))
+#define GPIO1_ICR2      *((volatile unsigned int*))
+#define GPIO1_IMR       *((volatile unsigned int*))
+#define GPIO1_ISR       *((volatile unsigned int*))
+#define GPIO1_EDGE_SEL  *((volatile unsigned int*))
+
+
+#endif
--- 汇编/README.md
+++
@@ -1,5 +0,0 @@
-要将imx6ull的IO作为GPIO使用，我们需要一下几步
-1.使能GPIO对应的时钟。
-2.设置寄存器IOMUXC_SW_MUX_CTL_PAD_XX_XX，设置IO的复用功能，使其复用为GPIO功能。
-3.设置寄存器IOMUXC_SW_PAD_CTL_PAD_XX_XX，设置IO的上下拉，速度等等。
-4.第2步已经将IO复用为了GPIO功能，所以需要配置GPIO，设置输入/输出、是否使用中断、默认输出电平等。
